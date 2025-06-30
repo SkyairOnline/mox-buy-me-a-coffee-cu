2e85a159d9188389fc6cc8a53d76fb9bc2d1bbc3
skyaironline <aldo@batamon.asia>
2025-06-30 10:55:24 +0700

    make deploy script

---
+++ script/deploy.py
@@ -0,0 +1,19 @@
+from moccasin.config import get_active_network
+from src import buy_me_a_coffee
+from moccasin.boa_tools import VyperContract
+
+def deploy_coffee(price_feed: str) -> VyperContract:
+    coffee: VyperContract = buy_me_a_coffee.deploy(price_feed)
+
+    active_network = get_active_network()
+    if active_network.has_explorer():
+        result = active_network.moccasin_verify(coffee)
+        result.wait_for_verification()
+    return coffee
+
+def moccasin_main() -> VyperContract:
+    active_network = get_active_network()
+    price_feed: VyperContract = active_network.manifest_named("price_feed")
+
+    print(f"Deploying Buy Me A Coffee on {active_network.name} with price feed at {price_feed.address}")
+    return deploy_coffee(price_feed)
